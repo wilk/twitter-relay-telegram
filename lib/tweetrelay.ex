305b177b7e032c5aae9d5de130c82e6e18033be6
wilk <wilk3ert@gmail.com>
2018-05-15 23:28:53 +0200

    Add some useless test

--- lib/tweetrelay.ex
+++ lib/tweetrelay.ex
@@ -1,31 +1,58 @@
 defmodule TweetRelay do
+  # macro "use" used to import GenServer and calling its __using__ method (GenServer.__using__())
   use GenServer
 
-  def start(:normal, args) do
+  def start(:normal, _) do
     IO.puts "start"
-    GenServer.start_link(__MODULE__, args)
+    GenServer.start_link(__MODULE__, %{})
   end
 
   # client
   def init(state) do
     IO.puts "init"
+    #{:ok, table} = :dets.open_file(:disk_storage, [type: :set])
+    #:dets.insert_new(table, {"meh", "asd", ["Elixir", "Go", "Rust"]})
+    #a = :dets.lookup(table, "meh")
+    #state = Map.put(state, :last_command, table)
+    #IO.inspect state
+    #IO.inspect a
+    {:ok, updates} =  Nadia.get_updates([limit: 1])
+    {:ok, last_command} = Enum.fetch(updates, 0)
+    state = Map.put(state, :last_command, last_command)
     schedule_work()
     {:ok, state}
   end
 
   defp schedule_work() do
     IO.puts "schedule_work"
-    Process.send_after(self(), :work, 2000)
+    Process.send_after(self(), :work, 1000)
   end
 
   # server
   def handle_info(:work, state) do
-    IO.puts "handle_info"
+    #IO.inspect state
+    #a = :dets.lookup(state[:table], "meh")
+    #IO.inspect a
+    #IO.puts "handle_info"
 
-    a = ExTwitter.search("@rep_tecno", [count: 5])
-      |> Enum.map(fn(tweet) -> tweet.text end)
-      |> Enum.join("\n-----\n")
-    Nadia.send_message(Application.get_env(:nadia, :chat_id), a)
+    #result = ExTwitter.search("@rep_tecno", [count: 5])
+    #  |> Enum.map(fn(tweet) -> IO.inspect tweet; tweet.text end)
+    #  |> Enum.join("\n-----\n")
+    #Nadia.send_message(Application.get_env(:nadia, :chat_id), result)
+
+    {:ok, updates} =  Nadia.get_updates([limit: 1, offset: state[:last_command].update_id+ 1])
+    #Enum.map(updates, fn(update) -> IO.inspect update end)
+    #IO.inspect commands
+
+    {:ok, last_command} = Enum.fetch(updates, 0)
+    IO.puts last_command.message.text
+    IO.puts state[:last_command].message.text
+    IO.puts last_command.update_id
+
+    if last_command.message.message_id != state[:last_command].message.message_id do
+      #Nadia.send_message(Application.get_env(:nadia, :chat_id), last_command.message.text)
+      state = Map.put(state, :last_command, last_command)
+    end
 
     schedule_work()
     {:noreply, state}
